9d20adc05e55a114083c6bce8439b0a7ca441704
Jack <rkdworn9411@gmail.com>
2021-11-03 15:37:33 +0900

    fix: ko -> eng

--- eventListen/trade.py
+++ eventListen/trade.py
@@ -28,20 +28,21 @@ mfi_bid = 10
 mfi_ask = 90
 
 tx_count = 1
+
 ### 0. 키움 로그인
-print("0. 키움 로그인")
+print("0. Kiwoom Login")
 kiwoom_conn = kiwoom.create_connect()
 
 # 0.1. 주문가능금액조회
-print("\n0.1. 주문가능금액조회")
+print("\n0.1. Get my amount")
 my_amount = kiwoom.get_amount(kiwoom_conn)
-print("주문가능금액: %s원" %(my_amount))
+print("My amount: %s원" %(my_amount))
 
 ### 1. 종목(cap >= 5천억 && KOSPI)조회 및 거래
-print("\n>> 1. 종목(cap >= 5천억 && KOSPI)조회 및 거래")
+print("\n>> 1. Get ticker(cap >= 500000000000 && KOSPI) & Trade")
 
 # 1.0. 매수종목조회
-print("\n1.0. 매수종목조회")
+print("\n1.0. Get bid basket")
 db = pymysql.connect(host=ADDR, port=int(PORT), user=ID, passwd=PW, db=DB, charset='utf8')
 cursor = db.cursor()
 cursor.execute("SELECT * FROM bid_basket")
@@ -52,13 +53,14 @@ if len(basketDf) > 0:
   print(basketDf.index)
 
 # 1.1. 기간 설정
-print("\n1.1. 기간 설정")
+print("\n1.1. Set the date")
 date = str(datetime.now().date()).replace('-', '')
+# date = "20211029"
 print("기간: (%s)" %(date))
 
 try:
   # 1.2. 티커별 종합정보조회
-  print("\n1.2. 티커별 종합정보조회")
+  print("\n1.2. Get info tickers")
   sql = """
   SELECT BOLMFI.*, CAP.cap
   FROM (
@@ -90,60 +92,58 @@ try:
   db.close()
   sumDf = pd.DataFrame(cursor.fetchall()).rename(columns={0:'date', 1:'ticker', 2:'period', 3:'close', 4:'low', 5:'medium', 6:'high', 7:'bandWidth', 8:'position', 9: 'tp', 10: 'mfi', 11: 'mfi_diff', 12:'cap'})
   sumDf.set_index('ticker', drop=True, inplace=True)
-
   for idx in sumDf.index:    
-    # break
     row = sumDf.loc[idx].copy()
     ticker = row.name
     ############################
     ## BID STEP    
     if row['position'] <= position_bid and row['mfi'] <= mfi_bid and row['mfi'] > 0 and row['mfi_diff'] > 0:
-      # 1.3. 매수기회포착
-      print("\n1.3. 매수기회포착 (%s)" %(ticker))
+      # 2.1. 매수기회포착
+      print("\n2.1. It's time to Bid!!! (%s)" %(ticker))
       bot.sendMessage(chat_id = chat_id, text="매수기회포착 (%s)" %(ticker))
-      # 1.4. 매수여부확인
-      print("1.4. 매수여부확인 (%s)" %(ticker))      
+      # 2.2. 매수여부확인
+      print("2.2. Check already bid (%s)" %(ticker))      
       if ticker in basketDf.index:
-        print("  [INFO] 이미 매수한 종목입니다. (%s)" %(ticker))
+        print("  [INFO] Already bid... (%s)" %(ticker))
         bot.sendMessage(chat_id = chat_id, text="[INFO] 이미 매수한 종목입니다. (%s)" %(ticker))        
       # 매수
       elif ticker not in basketDf.index and len(basketDf) < step:
         if my_amount >= trade_amount:
-          # 1.5. 매수
-          print("1.5. 매수 (%s)" %(ticker))      
-          print("  [INFO] 매수를 진행합니다. (%s)" %(ticker))
+          # 2.3. 매수
+          print("2.3. Bid (%s)" %(ticker))      
+          print("  [INFO] Let's Bid!! (%s)" %(ticker))
           bot.sendMessage(chat_id = chat_id, text="[INFO] 매수를 진행합니다. (%s)" %(ticker))
-          # 1.5.1. 매수수량계산
-          print("1.5.1. 매수수량계산 (%s)" %(ticker))      
+          # 2.3.1. 매수수량계산
+          print("2.3.1. Calculate bid volume (%s)" %(ticker))      
           price = row['close']
           volume = round(trade_amount // price)
           print("%s: %s원 %s주" %(ticker, price, volume))
           if volume > 0:
-            # 1.5.2. 키움증권매수
-            print("1.5.2. 키움증권매수 (%s)" %(ticker))                  
+            # 2.3.2. 키움증권매수
+            print("2.3.2. Bid by kiwoom (%s)" %(ticker))                  
             if kiwoom.get_status(kiwoom_conn):
               kiwoom.bid_market(kiwoom_conn, tx_count, ticker, volume)
             else:
               kiwoom_conn = kiwoom.create_connect()
               kiwoom.bid_market(kiwoom_conn, tx_count, ticker, volume)
-            # 1.5.3. 매수내역기록
-            print("1.5.3. 매수내역기록 (%s, %s)" %(date, ticker))    
+            # 2.3.3. 매수내역기록
+            print("2.3.3. Write history (%s, %s)" %(date, ticker))    
             db = pymysql.connect(host=ADDR, port=int(PORT), user=ID, passwd=PW, db=DB, charset='utf8')
             cursor = db.cursor()
             cursor.execute("INSERT INTO bid_basket (date, ticker, price, volume, mfi, position) VALUES (%s, '%s', %s, %s, %s, %s)" %(date, ticker, price, volume, row['mfi'], row['position']))
             cursor.execute("INSERT INTO history (date, ticker, status, bid_price, volume) VALUES (%s, '%s', 'bid', %s, %s)" %(date, ticker, price, volume))
             db.commit()
             db.close()
-            print("  [SUCCESS] 매수완료 (%s)" %(ticker))
+            print("  [SUCCESS] Complete the bid (%s)" %(ticker))
             bot.sendMessage(chat_id = chat_id, text="[SUCCESS] 매수완료 (%s)" %(ticker))
             bot.sendMessage(chat_id = chat_id, text="(date, ticker, status, bid_price, ask_price, volume, pnl, realized_pnl)")
             bot.sendMessage(chat_id = chat_id, text="(%s, %s, %s, %s, %s, %s, %s, %s)" %(date, ticker, 'bid', price, "NULL", volume, "NULL", "NULL"))
             tx_count += 1
           else:
-            print("  [ERROR] 금액이 %s원 이상입니다." %(trade_amount))
+            print("  [ERROR] Price is so expensive.. %s" %(trade_amount))
             bot.sendMessage(chat_id = chat_id, text="[ERROR] 금액이 %s원 이상입니다." %(trade_amount))
         else:
-          print("  [ERROR] 잔액이 부족합니다. 주문가능금액: %s원" %(my_amount))
+          print("  [ERROR] Not enough money. %s" %(my_amount))
           bot.sendMessage(chat_id = chat_id, text="[ERROR] 잔액이 부족합니다. 주문가능금액: %s원" %(my_amount))
     ############################
     ## ASK STEP    
@@ -153,33 +153,33 @@ try:
       bid_price = info['price']
       pnl = (1 - (bid_price / price)) * 100
       if row['position'] >= position_ask or row['mfi'] >= mfi_ask:
-        # 1.6. 매도기회포착
-        print("\n1.6. 매도기회포착 (%s)" %(ticker))
+        # 3.1. 매도기회포착
+        print("\n3.1. It's time to Ask!!! (%s)" %(ticker))
         bot.sendMessage(chat_id = chat_id, text="매도기회포착 (%s)" %(ticker))
-        # 1.7. 매도
-        print("1.7. 매도 (%s)" %(ticker))      
-        print("  [INFO] 매도를 진행합니다. (%s)" %(ticker))
+        # 3.2. 매도
+        print("3.2. Ask (%s)" %(ticker))      
+        print("  [INFO] Let's Ask!! (%s)" %(ticker))
         bot.sendMessage(chat_id = chat_id, text="[INFO] 매도를 진행합니다. (%s)" %(ticker))
-        # 1.7.1. 매도수량계산
-        print("1.7.1. 매도수량계산 (%s)" %(ticker))
+        # 3.2.1. 매도수량계산
+        print("3.2.1. Calculate ask volume (%s)" %(ticker))
         volume = info['volume']
         print("%s: %s원 %s주" %(ticker, price, volume))
-        # 1.7.1. 키움증권매도
-        print("1.7.2. 키움증권매도 (%s)" %(ticker))  
+        # 3.2.2. 키움증권매도
+        print("3.2.2. Ask by kiwoom (%s)" %(ticker))  
         if kiwoom.get_status(kiwoom_conn):
           kiwoom.ask_market(kiwoom_conn, tx_count, ticker, volume)
         else:
           kiwoom_conn = kiwoom.create_connect()
           kiwoom.ask_market(kiwoom_conn, tx_count, ticker, volume)
-        # 1.7.2. 매도내역기록
-        print("1.7.2. 거래내역기록 (%s, %s)" %(date, ticker))      
+        # 3.2.3. 매도내역기록
+        print("3.2.3. Write history (%s, %s)" %(date, ticker))      
         db = pymysql.connect(host=ADDR, port=int(PORT), user=ID, passwd=PW, db=DB, charset='utf8')
         cursor = db.cursor()
         cursor.execute("DELETE FROM bid_basket WHERE ticker = %s" %(ticker))
         cursor.execute("INSERT INTO history (date, ticker, status, bid_price, ask_price, volume, pnl, realized_pnl) VALUES (%s, '%s', 'ask', %s, %s, %s, %s, %s)" %(date, ticker, bid_price, price, volume, pnl, volume * pnl * 0.01))
         db.commit()
         db.close()
-        print("  [SUCCESS] 매도완료 (%s)" %(ticker))
+        print("  [SUCCESS] Complete the ask (%s)" %(ticker))
         bot.sendMessage(chat_id = chat_id, text="[SUCCESS] 매도완료 (%s)" %(ticker))
         bot.sendMessage(chat_id = chat_id, text="(date, ticker, status, bid_price, ask_price, volume, pnl, realized_pnl)")
         bot.sendMessage(chat_id = chat_id, text="(%s, %s, %s, %s, %s, %s, %s, %s)" %(date, ticker, 'ask', bid_price, price, volume, pnl, volume * pnl * 0.01))
