f9d1125ffe6f17fa241233418b552edfd921ff8d
rivernine <rivernine@github.com>
2021-11-17 13:40:06 +0900

    update: symbol list

--- global/stock.py
+++ global/stock.py
@@ -15,8 +15,13 @@ PW = 'root'
 
 nasdaq = list(fdr.StockListing('NASDAQ')['Symbol'])[:100]
 sp = list(fdr.StockListing('SP500')['Symbol'])[:100]
+cust = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'TSLA', 'FB', 'BRK-A', 'NVDA', 'V', 'JPM', 'JNJ', 'UNH', 'WMT', 'HD', 'BAC', 'MA', 'PG', 'DIS', 'ADBE', 'NFLX', 'PYPL', 'CRM', 'XOM', 'ORCL', 'NKE', 'CMCSA', 'PFE', 'TMO', 'LLY', 'KO'
+, 'CSCO', 'ABT', 'DHR', 'CVX', 'PEP', 'VZ', 'COST', 'AVGO', 'MRK', 'WFC', 'INTC', 'ABBV', 'TXN', 'MS', 'T', 'UPS', 'MCD', 'NEE', 'INTU', 'LOW', 'SCHW', 'UNP', 'PM', 'HON', 'QCOM', 'AMD', 'TMUS', 'AXP', 'MRNA', 'BLK', 'C'
+, 'GS', 'RTX', 'NOW', 'SBUX', 'CHTR', 'AMT', 'BMY', 'TGT', 'BA', 'ISRG', 'AMAT', 'SQ', 'EL', 'AMGN', 'CVS', 'GE', 'IBM', 'CAT', 'DE', 'SPGI', 'PLD', 'ABNB', 'MMM', 'ANTM', 'LMT', 'SYK', 'SNOW'
+, 'COP', 'ZTS', 'BKNG', 'BX', 'ADI', 'USB', 'ADP', 'PNC', 'MO', 'UBER', 'SNAP', 'TFC', 'DELL', 'MMC', 'COIN', 'GILD', 'MDLZ', 'GM', 'ZM', 'SHW' ]
 etc = ['NRGU', 'FNGU', 'TQQQ']
-symbols = list(set(nasdaq + sp + etc))
+# symbols = list(set(nasdaq + sp + etc))
+symbols = list(set(cust + etc))
 
 ### 1. 나스닥, SP500, 기타종목 최신화
 print(">> 1. Update Nasdq, sp500, etc")
