c124f724fd1f47c117bb2f66d3eb814077e7edc8
rivernine <rivernine@github.com>
2021-11-17 14:53:46 +0900

    add: coins

--- global/stock.py
+++ global/stock.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pykrx import stock
 import time
 from datetime import datetime, timedelta
+import ccxt
 
 ADDR = '192.168.56.100'
 PORT = '3306'
@@ -20,11 +21,12 @@ cust = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'TSLA', 'FB', 'BRK-A', 'NVDA', 'V', 'JPM
 , 'GS', 'RTX', 'NOW', 'SBUX', 'CHTR', 'AMT', 'BMY', 'TGT', 'BA', 'ISRG', 'AMAT', 'SQ', 'EL', 'AMGN', 'CVS', 'GE', 'IBM', 'CAT', 'DE', 'SPGI', 'PLD', 'ABNB', 'MMM', 'ANTM', 'LMT', 'SYK', 'SNOW'
 , 'COP', 'ZTS', 'BKNG', 'BX', 'ADI', 'USB', 'ADP', 'PNC', 'MO', 'UBER', 'SNAP', 'TFC', 'DELL', 'MMC', 'COIN', 'GILD', 'MDLZ', 'GM', 'ZM', 'SHW' ]
 etc = ['NRGU', 'FNGU', 'TQQQ']
+coins = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
 # symbols = list(set(nasdaq + sp + etc))
 symbols = list(set(cust + etc))
 
 ### 1. 나스닥, SP500, 기타종목 최신화
-print(">> 1. Update Nasdq, sp500, etc")
+print(">> 1. Update Nasdaq, sp500, etc")
 
 ### 2. 기존 데이터 제거
 print("2. Delete old data")
@@ -34,14 +36,25 @@ cursor.execute("TRUNCATE stocks_price")
 db.commit()
 
 ### 3. BTC
-df = fdr.DataReader('BTC/KRW', '2021-07-01').rename(columns={'Open': 'open', 'High':'high', 'Low':'low', 'Close':'close', 'Volume':'volume'}).drop(['Change'], axis=1)
-df['symbol'] = 'BTC/KRW'
-df = df[['symbol', 'open', 'high', 'low', 'close', 'volume']]
-db_connection = create_engine('mysql+pymysql://'+ ID +':'+ PW +'@'+ ADDR +':'+ PORT +'/'+ DB, encoding='utf-8', pool_pre_ping=True)
-conn = db_connection.connect()
-df.to_sql(name='stocks_price', con=db_connection, if_exists='append', index=True, index_label="date")
-conn.close()
-db.commit()
+binance = ccxt.binance()
+for coin in coins:
+  ohlcv = binance.fetch_ohlcv(coin, timeframe='1d', limit=100)
+  df = pd.DataFrame(ohlcv, columns = ['date', 'open', 'high', 'low', 'close', 'volume'])
+  df['date'] = pd.to_datetime(df['date'], unit='ms')
+  df = df.set_index(['date'])
+  db_connection = create_engine('mysql+pymysql://'+ ID +':'+ PW +'@'+ ADDR +':'+ PORT +'/'+ DB, encoding='utf-8', pool_pre_ping=True)
+  conn = db_connection.connect()
+  df.to_sql(name='stocks_price', con=db_connection, if_exists='append', index=True, index_label="date")
+  conn.close()
+  db.commit()  
+# df = fdr.DataReader('BTC/KRW', '2021-07-01').rename(columns={'Open': 'open', 'High':'high', 'Low':'low', 'Close':'close', 'Volume':'volume'}).drop(['Change'], axis=1)
+# df['symbol'] = 'BTC/KRW'
+# df = df[['symbol', 'open', 'high', 'low', 'close', 'volume']]
+# db_connection = create_engine('mysql+pymysql://'+ ID +':'+ PW +'@'+ ADDR +':'+ PORT +'/'+ DB, encoding='utf-8', pool_pre_ping=True)
+# conn = db_connection.connect()
+# df.to_sql(name='stocks_price', con=db_connection, if_exists='append', index=True, index_label="date")
+# conn.close()
+# db.commit()
 
 ### 4. GLOBAL
 print(symbols)
